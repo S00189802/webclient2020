af6071b4da38a86015aa6498f67a1d5d0b1b5894
Una LEstrange <lestrange.una@itsligo.ie>
2020-12-06 21:15:19 +0000

    auth guard working to check if logged in.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BookListComponent } from './book/book-list/book-list.component';
+import { AuthGuard } from './helpers/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'books',component: BookListComponent},
+  {path: 'books',component: BookListComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent}
 ];
 
